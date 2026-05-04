b26b0e5d67b631dbd31a12140e586a01d3f8f609
Sumit Yadav <sumityadav73397@gmail.com>
2026-05-04 22:17:59 +0530

    multi change in ports

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-     host: true,
+    host: "0.0.0.0",
     port: 3000,
     strictPort: true
   },
